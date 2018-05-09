f601f433c1de09bd38b2c90b049caa7f85e67e1d
Isfaaq <isfaaqg@gmail.com>
2018-05-09 16:51:38 +0400

    Slight fix

--- 0_GettingStarted/9/shortest.cpp
+++ 0_GettingStarted/9/shortest.cpp
@@ -1,3 +1,7 @@
 // What is the shortest valid program?
 // I'd say this
 int main(){return 0;}
+
+// Turns out the answer is:
+// int main(){}
+// url: https://stackoverflow.com/questions/4549214/shortest-program-in-c
